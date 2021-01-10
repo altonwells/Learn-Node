7780627632f7827d03225d06e15b060846d23c12
Alton Wells <Alton@Wells.ai>
2021-01-10 12:47:24 -0700

    More Features, Contollers & Mongo Methods
    MCV makes sense now.

--- genesis/controllers/storeController.js
+++ genesis/controllers/storeController.js
@@ -3,23 +3,42 @@ const Store = mongoose.model('Store');
 
 
 exports.homePage = (req, res) => {
-    console.log(req.name);
-    req.flash('error', 'Jee willikers Batman');
-    req.flash('info', 'Jee willikers Batman');
-    req.flash('warning', 'Jee willikers Batman');
-    req.flash('success', 'Jee willikers Batman');
     res.render('index');
 };
 
-exports.addStore = (req, res) => {
-    res.render('editStore', {
+exports.addStore = async (req, res) => {
+   await  res.render('editStore', {
         title: "Add Store"
     });
 }
 
 exports.createStore = async (req, res) => {
-    const store = new Store(req.body);
-    await store.save();
-    req.flash('success',`You've successfully passed something to a database...it's not that impressive...' ${store.name} Just leave us a review.`)
-    res.redirect('/');
+    const store = await (new Store(req.body)).save();
+    req.flash('success',`${store.name}...You've successfully passed something to a database...it's not that impressive...'`)
+    res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+    const stores =  await Store.find();
+    res.render('stores', {title: 'Stores', stores});
+};
+
+exports.editStore = async (req, res) => {
+    const store = await Store.findOne({_id: req.params.id});
+    res.render('editStore',{
+        title: store.name,
+        store
+    })
+
+};
+
+exports.updateStore = async (req, res) => {
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true, // return the new store instead of the old one
+        runValidators: true
+      }).exec();
+      req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+      res.redirect(`/stores/${store._id}/edit`);
+}
+
+
--- genesis/routes/index.js
+++ genesis/routes/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const storeController = require('../controllers/storeController')
+const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers')
 
 // Do work here
-router.get('/', storeController.homePage);
-router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
-  
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+// Store Routers 
+router.get('/', storeController.getStores);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.post('/add/:id', catchErrors(storeController.updateStore))
+
 
 module.exports = router;
